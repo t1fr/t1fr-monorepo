c722364f0353870c6619299b08d32dff2145c6b8
HI_OuO <zvn2060@gmail.com>
2023-09-11 20:09:42 +0800

    增加 /join 的表達性，跳脫 markdown，tag 申請人

--- src/constant.ts
+++ src/constant.ts
@@ -1,7 +1,16 @@
+import * as process from "process";
+
 export const SquadronMemberListUrl = "https://warthunder.com/en/community/claninfo/The%20First%20Frontline%20Rangers";
 
 export const DiscordRole = {
 	聯隊戰隊員: "1046629026355228762",
 	聯隊戰身分群: "1054766361991204864",
 	休閒隊員: "1103384315599015987",
+};
+
+const TestChannel = "1104331400993312828";
+
+export const Channel = {
+	入隊須知: "1145362065813405776",
+	入隊申請窗口: process.env.NODE_ENV === "production" ? "1046829250101117038" : TestChannel,
 };
