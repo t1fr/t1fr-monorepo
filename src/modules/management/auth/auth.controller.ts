ea422c4719fa241c1586da5fa2a00556058b4dee
HI_OuO <zvn2060@gmail.com>
2023-10-05 01:25:52 +0800

    fix: 調整 Oauth 流程為 redirect 到後端 endpoint 後設置 cookie 在導回離開時頁面，更改端口到 6518

--- src/modules/management/auth/auth.controller.ts
+++ src/modules/management/auth/auth.controller.ts
@@ -1,29 +1,33 @@
-import { Body, Controller, Delete, Post, Res, UseGuards } from "@nestjs/common";
+import { Controller, Delete, Get, Post, Query, Req, Res, UseGuards } from "@nestjs/common";
 import { AuthService } from "@/modules/management/auth/auth.service";
-import { CookieOptions, Response } from "express";
+import { CookieOptions, Request, Response } from "express";
 import { JwtGuard } from "@/guards/jwt.guard";
 import { User } from "@/decorators/user.decorator";
 import { Member } from "@/modules/management/member/member.schema";
 import * as process from "process";
+import { ConfigService } from "@nestjs/config";
 
 @Controller("auth")
 export class AuthController {
-	constructor(private readonly authService: AuthService) {}
+	constructor(
+		private readonly authService: AuthService,
+		private readonly configService: ConfigService,
+	) {}
 
 	static cookieOptions: CookieOptions = {
 		httpOnly: true,
 		secure: true,
 		domain: process.env["NODE_ENV"] === "test" ? "localhost" : ".web.app",
 		path: "/",
-		sameSite: "strict",
+		sameSite: "none",
 		maxAge: 1800 * 1000,
 	};
 
-	@Post()
-	async login(@Body("code") code: string, @Res({ passthrough: true }) response: Response) {
-		const { token, data } = await this.authService.login(code);
+	@Get("redirect")
+	async redirect(@Query("code") code: string, @Res({ passthrough: true }) response: Response, @Req() request: Request) {
+		const token = await this.authService.login(code);
 		response.cookie("token", token, AuthController.cookieOptions);
-		return data;
+		response.redirect(this.configService.getOrThrow("frontend_redirect_url"));
 	}
 
 	@UseGuards(JwtGuard)
