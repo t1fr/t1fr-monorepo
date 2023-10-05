a743b21ba490d562f706366af30e08cd81f9dffb
HI_OuO <zvn2060@gmail.com>
2023-10-05 11:21:48 +0800

    fix: use backend host as cookie domain

--- src/modules/management/auth/auth.controller.ts
+++ src/modules/management/auth/auth.controller.ts
@@ -1,30 +1,20 @@
-import { Controller, Delete, Get, Post, Query, Req, Res, UseGuards } from "@nestjs/common";
+import { Controller, Delete, Get, Post, Query, Res, UseGuards } from "@nestjs/common";
 import { AuthService } from "@/modules/management/auth/auth.service";
 import { CookieOptions, Response } from "express";
 import { JwtGuard } from "@/guards/jwt.guard";
 import { User } from "@/decorators/user.decorator";
 import { Member } from "@/modules/management/member/member.schema";
-import { ConfigService } from "@nestjs/config";
 
 @Controller("auth")
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
-	static cookieOptions: CookieOptions = {
-		httpOnly: true,
-		secure: true,
-		path: "/",
-		sameSite: "none",
-		maxAge: 1800 * 1000,
-	};
+	static cookieOptions: CookieOptions = { httpOnly: true, secure: true, path: "/", sameSite: "none", maxAge: 1800 * 1000, domain: AuthService.Host };
 
 	@Get("redirect")
 	async redirect(@Query("code") code: string, @Query("state") state: string, @Res({ passthrough: true }) response: Response) {
 		const redirect = decodeURIComponent(state);
-		if (code) {
-			const token = await this.authService.login(code);
-			response.cookie("token", token, { ...AuthController.cookieOptions, domain: new URL(redirect).hostname });
-		}
+		if (code) response.cookie("token", await this.authService.login(code), AuthController.cookieOptions);
 		response.redirect(redirect);
 	}
 
@@ -36,6 +26,6 @@ export class AuthController {
 
 	@Delete()
 	async logout(@Res({ passthrough: true }) response: Response) {
-		response.cookie("token", "", { maxAge: 0 });
+		response.cookie("token", "", { ...AuthController.cookieOptions, maxAge: 0 });
 	}
 }
