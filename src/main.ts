e3403a0d58dd6b7ff41ab9c5f6af51d0d95e1180
HI_OuO <zvn2060@gmail.com>
2023-11-18 00:26:27 +0800

    fix: CORS allow origin http://t1fr.club to https

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ dayjs.extend(utc);
 const allowedOrigin: CustomOrigin = (origin: string, callback) => {
 	if (!origin) return callback(null, false);
 	if (origin.match(/http:\/\/localhost.*/)) return callback(null, true);
-	if (origin.match(/http:\/\/t1fr\.club\/.*/)) return callback(null, true);
+	if (origin.match(/https:\/\/t1fr\.club\/.*/)) return callback(null, true);
 	return callback(new Error(`請求的來源: ${origin} 不在白名單中`), false);
 };
 
