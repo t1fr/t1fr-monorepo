f38f5b0caa0ed96cecea182bd07a1a032d9df547
HI_OuO <zvn2060@gmail.com>
2024-01-02 18:17:38 +0800

    fix: 修正顯示統計資訊點數 log comment 為 null 時以"無"代替

--- src/utlity.ts
+++ src/utlity.ts
@@ -1,21 +1,21 @@
-import { ActionRowBuilder, APIEmbedField, ModalActionRowComponentBuilder, RestOrArray } from "discord.js";
+import {ActionRowBuilder, APIEmbedField, ModalActionRowComponentBuilder, RestOrArray} from "discord.js";
 
 export function arrayToChoices(array: readonly string[]) {
-	return array.map(value => ({ name: value, value }));
+    return array.map(value => ({name: value, value}));
 }
 
 export function configLayout(layout: ModalActionRowComponentBuilder[]): ActionRowBuilder<ModalActionRowComponentBuilder>[] {
-	return layout.map(value => new ActionRowBuilder<ModalActionRowComponentBuilder>().setComponents(value));
+    return layout.map(value => new ActionRowBuilder<ModalActionRowComponentBuilder>().setComponents(value));
 }
 
 export function selectToFields<T>(array: T[], selection: (keyof T)[], headers: string[]): RestOrArray<APIEmbedField> {
-	const result = selection.reduce((acc, cur) => ({ ...acc, [cur]: [] }), {} as { [key in keyof T]: string[] });
-	if (!array.length) return [];
-	array.forEach(value => {
-		for (const key of selection) result[key].push(`${value[key]}`);
-	});
+    const result = selection.reduce((acc, cur) => ({...acc, [cur]: []}), {} as { [key in keyof T]: string[] });
+    if (!array.length) return [];
+    array.forEach(value => {
+        for (const key of selection) result[key].push(`${value[key] ?? "無"}`);
+    });
 
-	return headers.map((header, index) => ({ name: header, value: result[selection[index]].join("\n"), inline: true }));
+    return headers.map((header, index) => ({name: header, value: result[selection[index]].join("\n"), inline: true}));
 }
 
 
