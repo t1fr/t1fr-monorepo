df266c28b8e5f436a3c883e51e450e2b461efcaa
HI_OuO <zvn2060@gmail.com>
2024-01-04 20:33:52 +0800

    feat: 回傳最新賽季的行程表 API

--- src/modules/schedule/schedule.service.ts
+++ src/modules/schedule/schedule.service.ts
@@ -1,189 +1,194 @@
-import { Injectable, Logger } from "@nestjs/common";
-import { Cron, CronExpression } from "@nestjs/schedule";
-import { ChannelType, Client } from "discord.js";
-import { InjectModel } from "@nestjs/mongoose";
-import { Season, Section } from "@/modules/schedule/season.schema";
-import { ConnectionName } from "@/constant";
-import { Model } from "mongoose";
+import {Injectable, Logger} from "@nestjs/common";
+import {Cron, CronExpression} from "@nestjs/schedule";
+import {ChannelType, Client} from "discord.js";
+import {InjectModel} from "@nestjs/mongoose";
+import {Season, Section} from "@/modules/schedule/season.schema";
+import {ConnectionName} from "@/constant";
+import {Model} from "mongoose";
 import dayjs from "dayjs";
-import { AccountService } from "@/modules/management/account/account.service";
+import {AccountService} from "@/modules/management/account/account.service";
 import * as process from "process";
-import { HttpService } from "@nestjs/axios";
-import { LeaderboardData, processLeaderboardData } from "@/modules/schedule/leaderboard.data";
+import {HttpService} from "@nestjs/axios";
+import {LeaderboardData, processLeaderboardData} from "@/modules/schedule/leaderboard.data";
 import Range from "lodash/range";
-import { Backup } from '@/modules/management/backup.interface'
-import { GithubService } from '@/modules/github/github.service'
+import {Backup} from '@/modules/management/backup.interface'
+import {GithubService} from '@/modules/github/github.service'
 
 @Injectable()
-export class ScheduleService implements Backup{
-	private static readonly logger = new Logger(ScheduleService.name);
+export class ScheduleService implements Backup {
+    private static readonly logger = new Logger(ScheduleService.name);
 
-	constructor(
-		@InjectModel(Season.name, ConnectionName.Management) private readonly seasonModel: Model<Season>,
-		private readonly accountService: AccountService,
-		private readonly client: Client,
-		private readonly httpService: HttpService,
-		private readonly githubService: GithubService
-	) {}
+    constructor(
+        @InjectModel(Season.name, ConnectionName.Management) private readonly seasonModel: Model<Season>,
+        private readonly accountService: AccountService,
+        private readonly client: Client,
+        private readonly httpService: HttpService,
+        private readonly githubService: GithubService
+    ) {
+    }
 
-	async upsertSeason(year: string, raw: string) {
-		const sections = ScheduleService.parseTextToSections(parseInt(year), raw);
-		sections.sort((a, b) => dayjs(a.from).diff(b.from));
-		await this.seasonModel.findOneAndUpdate(
-			{ year: sections[0].from.getFullYear(), season: sections[0].from.getMonth() / 2 + 1 },
-			{ sections: sections },
-			{ upsert: true },
-		);
-	}
+    async upsertSeason(year: string, raw: string) {
+        const sections = ScheduleService.parseTextToSections(parseInt(year), raw);
+        sections.sort((a, b) => dayjs(a.from).diff(b.from));
+        await this.seasonModel.findOneAndUpdate(
+            {year: sections[0].from.getFullYear(), season: sections[0].from.getMonth() / 2 + 1},
+            {sections: sections},
+            {upsert: true},
+        );
+    }
 
-	async getCurrentBattleRating() {
-		return new Promise<number | null>(async resolve => {
-			const { now, year, season } = ScheduleService.CurrentSeason;
-			const sections = await this.seasonModel.aggregate<{ battleRating: number }>([
-				{ $match: { year, season } },
-				{ $unwind: "$sections" },
-				{ $match: { "sections.from": { $lte: now }, "sections.to": { $gt: now } } },
-				{ $project: { _id: 0, battleRating: "$sections.battleRating" } },
-			]);
+    async getLatestSeason() {
+        return this.seasonModel.findOne({}, {season: true, year: true, sections: true}, {sort: {_id: -1}}).exec()
+    }
 
-			if (sections.length) resolve(sections[0].battleRating);
-			resolve(null);
-		});
-	}
+    async getCurrentBattleRating() {
+        return new Promise<number | null>(async resolve => {
+            const {now, year, season} = ScheduleService.CurrentSeason;
+            const sections = await this.seasonModel.aggregate<{ battleRating: number }>([
+                {$match: {year, season}},
+                {$unwind: "$sections"},
+                {$match: {"sections.from": {$lte: now}, "sections.to": {$gt: now}}},
+                {$project: {_id: 0, battleRating: "$sections.battleRating"}},
+            ]);
 
-	static get CurrentSeason() {
-		const now = new Date();
-		const year = now.getUTCFullYear();
-		const season = Math.floor(now.getUTCMonth() / 2) + 1;
-		return { now, year, season };
-	}
+            if (sections.length) resolve(sections[0].battleRating);
+            resolve(null);
+        });
+    }
 
-	async getCurrentSeasonTable() {
-		const { year, season } = ScheduleService.CurrentSeason;
-		const found = await this.seasonModel.findOne({ year, season });
+    static get CurrentSeason() {
+        const now = new Date();
+        const year = now.getUTCFullYear();
+        const season = Math.floor(now.getUTCMonth() / 2) + 1;
+        return {now, year, season};
+    }
 
-		if (!found) return Promise.reject("查無當前賽季資料");
+    async getCurrentSeasonTable() {
+        const {year, season} = ScheduleService.CurrentSeason;
+        const found = await this.seasonModel.findOne({year, season});
 
-		return ScheduleService.seasonToTable(found);
-	}
+        if (!found) return Promise.reject("查無當前賽季資料");
 
-	@Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, { utcOffset: 0 })
-	async updateBulletin() {
-		if (process.env["NODE_ENV"] === "test") return;
-		const battleRating = await this.getCurrentBattleRating();
-		const messages = { category: "聯隊戰", announcement: "今日分房：新賽季" };
-		if (battleRating) {
-			const brString = battleRating.toFixed(1).replace(/\d/g, digitToFullwidth);
-			messages.announcement = `今日分房：${brString}`;
-			messages.category = `聯隊戰：${brString}`;
-		}
-		const category = this.client.channels.resolve("1046624503276515339");
-		if (category && category.type === ChannelType.GuildCategory) {
-			category.setName(messages.category);
-		}
+        return ScheduleService.seasonToTable(found);
+    }
 
-		const channel = this.client.channels.resolve("1047751571708051486");
-		if (channel && channel.type === ChannelType.GuildVoice) {
-			channel.setName(messages.announcement);
-		}
+    @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {utcOffset: 0})
+    async updateBulletin() {
+        if (process.env["NODE_ENV"] === "test") return;
+        const battleRating = await this.getCurrentBattleRating();
+        const messages = {category: "聯隊戰", announcement: "今日分房：新賽季"};
+        if (battleRating) {
+            const brString = battleRating.toFixed(1).replace(/\d/g, digitToFullwidth);
+            messages.announcement = `今日分房：${brString}`;
+            messages.category = `聯隊戰：${brString}`;
+        }
+        const category = this.client.channels.resolve("1046624503276515339");
+        if (category && category.type === ChannelType.GuildCategory) {
+            category.setName(messages.category);
+        }
 
-		ScheduleService.logger.log("更新聯隊戰分房");
+        const channel = this.client.channels.resolve("1047751571708051486");
+        if (channel && channel.type === ChannelType.GuildVoice) {
+            channel.setName(messages.announcement);
+        }
 
-		return battleRating !== undefined;
-	}
+        ScheduleService.logger.log("更新聯隊戰分房");
 
-	private static seasonToTable(currentSeason: Season) {
-		const { year, season, sections } = currentSeason;
-		const startMonth = (season - 1) * 2 + 1;
-		const scheduleMessage = [
-			`**${year} 年 ${startMonth} ~ ${startMonth + 1} 月**聯隊戰行程`,
-			"```",
-			"╭───────┬───────┬──────────╮",
-			"│ Start │  End  │  Max BR  │",
-			"├───────┼───────┼──────────┤",
-		];
+        return battleRating !== undefined;
+    }
 
-		const sectionRows = sections.map(section => {
-			const startString = dayjs(section.from).format("MM/DD");
-			const endString = dayjs(section.to).subtract(1, "day").format("MM/DD");
-			const battleRatingString = section.battleRating.toFixed(1).padStart(6);
-			return `│ ${startString} │ ${endString} │  ${battleRatingString}  │`;
-		});
+    private static seasonToTable(currentSeason: Season) {
+        const {year, season, sections} = currentSeason;
+        const startMonth = (season - 1) * 2 + 1;
+        const scheduleMessage = [
+            `**${year} 年 ${startMonth} ~ ${startMonth + 1} 月**聯隊戰行程`,
+            "```",
+            "╭───────┬───────┬──────────╮",
+            "│ Start │  End  │  Max BR  │",
+            "├───────┼───────┼──────────┤",
+        ];
 
-		sectionRows.forEach(sectionRow => {
-			scheduleMessage.push(sectionRow, "├───────┼───────┼──────────┤");
-		});
+        const sectionRows = sections.map(section => {
+            const startString = dayjs(section.from).format("MM/DD");
+            const endString = dayjs(section.to).subtract(1, "day").format("MM/DD");
+            const battleRatingString = section.battleRating.toFixed(1).padStart(6);
+            return `│ ${startString} │ ${endString} │  ${battleRatingString}  │`;
+        });
 
-		scheduleMessage[scheduleMessage.length - 1] = "╰───────┴───────┴──────────╯";
-		scheduleMessage.push("```");
+        sectionRows.forEach(sectionRow => {
+            scheduleMessage.push(sectionRow, "├───────┼───────┼──────────┤");
+        });
 
-		return scheduleMessage.join("\n");
-	}
+        scheduleMessage[scheduleMessage.length - 1] = "╰───────┴───────┴──────────╯";
+        scheduleMessage.push("```");
 
-	isLastDayOfMonth(now: Date) {
-		const lastDayOfMonth = dayjs.utc().endOf("month");
-		return lastDayOfMonth.isSame(now, "day");
-	}
+        return scheduleMessage.join("\n");
+    }
 
-	@Cron(CronExpression.EVERY_DAY_AT_11PM, { utcOffset: 0 })
-	async snapshot(force: boolean = false) {
-		const { now, year, season } = ScheduleService.CurrentSeason;
-		if (!force && !this.isLastDayOfMonth(now)) return;
-		const accounts = await this.accountService.listAccounts();
+    isLastDayOfMonth(now: Date) {
+        const lastDayOfMonth = dayjs.utc().endOf("month");
+        return lastDayOfMonth.isSame(now, "day");
+    }
 
-		const top100squads = await this.scrapLeaderboard(1, 5);
-		let position = null;
-		let t1fr = top100squads.find(squad => squad._id === 1078072);
-		if (t1fr) {
-			position = t1fr.pos;
-		} else {
-			const top100to200squads = await this.scrapLeaderboard(6, 5);
-			t1fr = top100to200squads.find(squad => squad._id === 1078072);
-			if (t1fr) position = t1fr.pos;
-		}
+    @Cron(CronExpression.EVERY_DAY_AT_11PM, {utcOffset: 0})
+    async snapshot(force: boolean = false) {
+        const {now, year, season} = ScheduleService.CurrentSeason;
+        if (!force && !this.isLastDayOfMonth(now)) return;
+        const accounts = await this.accountService.listAccounts();
 
-		await this.seasonModel.updateOne({ year, season }, { $set: { accounts, finalPos: position, top100squads: top100squads.map(processLeaderboardData) } });
-	}
+        const top100squads = await this.scrapLeaderboard(1, 5);
+        let position = null;
+        let t1fr = top100squads.find(squad => squad._id === 1078072);
+        if (t1fr) {
+            position = t1fr.pos;
+        } else {
+            const top100to200squads = await this.scrapLeaderboard(6, 5);
+            t1fr = top100to200squads.find(squad => squad._id === 1078072);
+            if (t1fr) position = t1fr.pos;
+        }
 
-	async scrapLeaderboard(startPage: number, total: number) {
-		const queryUrl = (page: number) => `https://warthunder.com/en/community/getclansleaderboard/dif/_hist/page/${page}/sort/dr_era5`;
+        await this.seasonModel.updateOne({year, season}, {$set: {accounts, finalPos: position, top100squads: top100squads.map(processLeaderboardData)}});
+    }
 
-		const response = await Promise.all(
-			Range(startPage, startPage + total).map(page => {
-				return new Promise<LeaderboardData[]>(async resolve => {
-					const data = await this.httpService.axiosRef.get<{ data: LeaderboardData[] }>(queryUrl(page));
-					resolve(data.data.data);
-				});
-			}),
-		);
+    async scrapLeaderboard(startPage: number, total: number) {
+        const queryUrl = (page: number) => `https://warthunder.com/en/community/getclansleaderboard/dif/_hist/page/${page}/sort/dr_era5`;
 
-		return response.reduce((acc, cur) => [...acc, ...cur], []);
-	}
+        const response = await Promise.all(
+            Range(startPage, startPage + total).map(page => {
+                return new Promise<LeaderboardData[]>(async resolve => {
+                    const data = await this.httpService.axiosRef.get<{ data: LeaderboardData[] }>(queryUrl(page));
+                    resolve(data.data.data);
+                });
+            }),
+        );
 
-	static parseTextToSections(year: number, scheduleText: string): Section[] {
-		return scheduleText
-			.split("\n")
-			.map(line => line.match(/(\d*\.\d*)\s*\((\d*\.\d*).*?(\d*\.\d*)\)/))
-			.map(matches => {
-				if (!matches) throw new Error("提供的賽季論壇訊息不合格式");
-				return {
-					from: dayjs.utc(`${year}.${matches[2]}`, "YYYY.DD.MM").toDate(),
-					to: dayjs.utc(`${year}.${matches[3]}`, "YYYY.DD.MM").add(1, "day").toDate(),
-					battleRating: parseFloat(matches[1]),
-				};
-			});
-	}
+        return response.reduce((acc, cur) => [...acc, ...cur], []);
+    }
 
-	@Cron("50 07 30 * *", { utcOffset: 0 })
-	async backup() {
-		const seasons = await this.seasonModel.find().sort({ year: -1, season: -1 }).limit(3);
-		const content = JSON.stringify(seasons.map(season => season.toObject()));
-		await this.githubService.upsertFile("season.json", content);
-	}
+    static parseTextToSections(year: number, scheduleText: string): Section[] {
+        return scheduleText
+            .split("\n")
+            .map(line => line.match(/(\d*\.\d*)\s*\((\d*\.\d*).*?(\d*\.\d*)\)/))
+            .map(matches => {
+                if (!matches) throw new Error("提供的賽季論壇訊息不合格式");
+                return {
+                    from: dayjs.utc(`${year}.${matches[2]}`, "YYYY.DD.MM").toDate(),
+                    to: dayjs.utc(`${year}.${matches[3]}`, "YYYY.DD.MM").add(1, "day").toDate(),
+                    battleRating: parseFloat(matches[1]),
+                };
+            });
+    }
+
+    @Cron("50 07 30 * *", {utcOffset: 0})
+    async backup() {
+        const seasons = await this.seasonModel.find().sort({year: -1, season: -1}).limit(3);
+        const content = JSON.stringify(seasons.map(season => season.toObject()));
+        await this.githubService.upsertFile("season.json", content);
+    }
 }
 
-const digitFullwidthMap = { "0": "０", "1": "１", "2": "２", "3": "３", "4": "４", "5": "５", "6": "６", "7": "７", "8": "８", "9": "９" };
+const digitFullwidthMap = {"0": "０", "1": "１", "2": "２", "3": "３", "4": "４", "5": "５", "6": "６", "7": "７", "8": "８", "9": "９"};
 
-function digitToFullwidth (digit: keyof typeof digitFullwidthMap) {
-  return digitFullwidthMap[digit]
+function digitToFullwidth(digit: keyof typeof digitFullwidthMap) {
+    return digitFullwidthMap[digit]
 }
