c7c5b48487fb2a077b890f92ced7ffd44b9283e6
HI_OuO <zvn2060@gmail.com>
2024-01-11 18:17:12 +0800

    ci: 部屬到 KCL 機器 by ssh remote command

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,65 +1,65 @@
-import {Module} from "@nestjs/common";
-import {ConfigModule, ConfigService} from "@nestjs/config";
-import {NecordModule} from "necord";
-import {IntentsBitField} from "discord.js";
-import {MongooseModule} from "@nestjs/mongoose";
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { NecordModule } from "necord";
+import { IntentsBitField } from "discord.js";
+import { MongooseModule } from "@nestjs/mongoose";
 import LoggerModule from "@/modules/logging/logger.module";
-import {ConnectionName} from "@/constant";
-import {HttpModule} from "@nestjs/axios";
-import {ScheduleModule} from "@nestjs/schedule";
-import {DiscordBotModule} from "@/modules/bot/bot.module";
-import {AuthModule} from '@/modules/auth/auth.module'
-import {CqrsModule} from "@nestjs/cqrs";
+import { ConnectionName } from "@/constant";
+import { HttpModule } from "@nestjs/axios";
+import { ScheduleModule } from "@nestjs/schedule";
+import { DiscordBotModule } from "@/modules/bot/bot.module";
+import { AuthModule } from "@/modules/auth/auth.module";
+import { CqrsModule } from "@nestjs/cqrs";
 
 const intents = [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildVoiceStates,
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
+	IntentsBitField.Flags.Guilds,
+	IntentsBitField.Flags.GuildVoiceStates,
+	IntentsBitField.Flags.Guilds,
+	IntentsBitField.Flags.GuildMembers,
+	IntentsBitField.Flags.GuildMessages,
+	IntentsBitField.Flags.MessageContent,
 ];
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({isGlobal: true}),
-        {...HttpModule.register({}), global: true},
-        ScheduleModule.forRoot(),
-        MongooseModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                uri: "mongodb://220.133.81.52:38422",
-                user: "***REMOVED***",
-                pass: "***REMOVED***",
-                dbName: configService.getOrThrow("DATABASE"),
-                authSource: "admin",
-            }),
-            connectionName: ConnectionName.Management,
-        }),
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		{ ...HttpModule.register({}), global: true },
+		ScheduleModule.forRoot(),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				uri: "mongodb://220.133.81.52:38422",
+				user: "***REMOVED***",
+				pass: "***REMOVED***",
+				dbName: configService.getOrThrow("DATABASE"),
+				authSource: "admin",
+			}),
+			connectionName: ConnectionName.Management,
+		}),
 
-        MongooseModule.forRoot("mongodb://220.133.81.52:38422", {
-            user: "***REMOVED***",
-            pass: "***REMOVED***",
-            dbName: "common",
-            authSource: "admin",
-            connectionName: ConnectionName.Common,
-        }),
+		MongooseModule.forRoot("mongodb://220.133.81.52:38422", {
+			user: "***REMOVED***",
+			pass: "***REMOVED***",
+			dbName: "common",
+			authSource: "admin",
+			connectionName: ConnectionName.Common,
+		}),
 
-        NecordModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                token: configService.getOrThrow("BOT_TOKEN"),
-                intents: intents,
-                development: ["1046623840710705152"],
-            }),
-        }),
-        CqrsModule.forRoot(),
-        DiscordBotModule,
-        AuthModule,
-        LoggerModule,
-    ],
+		NecordModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				token: configService.getOrThrow("BOT_TOKEN"),
+				intents: intents,
+				development: ["1046623840710705152"],
+			}),
+		}),
+		CqrsModule.forRoot(),
+		DiscordBotModule,
+		AuthModule,
+		LoggerModule,
+	],
 })
 export class AppModule {
 }
