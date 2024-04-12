f6ecba0ec374eab997ea3b010fae918b63e4f550
HI_OuO <zvn2060@gmail.com>
2024-04-12 23:58:25 +0800

    feat: Add an abstract domain error class

--- libs/backend/wiki/src/index.ts
+++ libs/backend/wiki/src/index.ts
@@ -3,4 +3,3 @@ export * from "./application/command/ScapeDatamine";
 export * from "./application/query/Search";
 export * from "./application/query/FindById";
 export * from "./application/query/ListEnumableField";
-export * from "./domain/DomainErrorType";
