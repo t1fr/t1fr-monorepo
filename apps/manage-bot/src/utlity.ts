34de83423f965988ff6c6c7edae14e99754fcf28
HI_OuO <zvn2060@gmail.com>
2024-04-23 22:27:57 +0800

    refactor: 簡化設定 Modal layout, 改成參數列表以及不用調用 TextInputBuilder 了

--- apps/manage-bot/src/utlity.ts
+++ apps/manage-bot/src/utlity.ts
@@ -1,21 +1,23 @@
-import { ActionRowBuilder, APIEmbedField, ModalActionRowComponentBuilder, RestOrArray } from "discord.js";
+import { ActionRowBuilder, APIEmbedField, ModalActionRowComponentBuilder, RestOrArray, TextInputBuilder } from "discord.js";
 
 export function arrayToChoices(array: readonly string[]) {
-	return array.map(value => ({ name: value, value }));
+    return array.map(value => ({ name: value, value }));
 }
 
-export function configLayout(layout: ModalActionRowComponentBuilder[]): ActionRowBuilder<ModalActionRowComponentBuilder>[] {
-	return layout.map(value => new ActionRowBuilder<ModalActionRowComponentBuilder>().setComponents(value));
+type TextInputBuilderParameter = ConstructorParameters<typeof TextInputBuilder>[0]
+
+export function configLayout(...layout: TextInputBuilderParameter[]): ActionRowBuilder<ModalActionRowComponentBuilder>[] {
+    return layout.map(value => new ActionRowBuilder<ModalActionRowComponentBuilder>().setComponents(new TextInputBuilder(value)));
 }
 
 export function selectToFields<T>(array: T[], selection: (keyof T)[], headers: string[]): RestOrArray<APIEmbedField> {
-	const result = selection.reduce((acc, cur) => ({ ...acc, [cur]: [] }), {} as { [key in keyof T]: string[] });
-	if (!array.length) return [];
-	array.forEach(value => {
-		for (const key of selection) result[key].push(`${value[key] ?? "無"}`);
-	});
+    const result = selection.reduce((acc, cur) => ({ ...acc, [cur]: [] }), {} as { [key in keyof T]: string[] });
+    if (!array.length) return [];
+    array.forEach(value => {
+        for (const key of selection) result[key].push(`${value[key] ?? "無"}`);
+    });
 
-	return headers.map((header, index) => ({ name: header, value: result[selection[index]].join("\n"), inline: true }));
+    return headers.map((header, index) => ({ name: header, value: result[selection[index]].join("\n"), inline: true }));
 }
 
 
