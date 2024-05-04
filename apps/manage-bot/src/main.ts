344c3c6864810a18b1cf27c424eb942b05a34160
HI_OuO <zvn2060@gmail.com>
2024-05-04 09:37:09 +0800

    build: 後端全面轉用 Vite

--- apps/manage-bot/src/main.ts
+++ apps/manage-bot/src/main.ts
@@ -1,12 +1,21 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./AppModule";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import "tslib";
+import { AppModule } from "./AppModule";
 
-async function bootstrap() {
-    const app = await NestFactory.createApplicationContext(AppModule);
-    app.init();
-    Logger.log(`🚀 Discord Bot is running`);
+async function createApp(standaloneApp: boolean) {
+    const app = await (standaloneApp ? NestFactory.createApplicationContext(AppModule) : NestFactory.create<NestExpressApplication>(AppModule));
+    return app;
 }
 
-bootstrap();
+async function bootstrap() {
+    const app = await createApp(true)
+    app.init();
+    Logger.log(`🚀 聯隊管理 BOT 執行中`);
+}
+
+if (__BUILD__) bootstrap();
+
+
+export const appServer = createApp(false)
