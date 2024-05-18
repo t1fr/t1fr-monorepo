6e7eebf8d436c8ac2b91171da8782ac0f6570edd
HI_OuO <zvn2060@gmail.com>
2024-05-19 01:26:29 +0800

    feat: 設置 Hud Event 監聽器 前端 lib

---
+++ libs/frontend/hud-event-watcher/vite.config.ts
@@ -0,0 +1,25 @@
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+
+export default defineConfig({
+    root: __dirname,
+    cacheDir: "../../../node_modules/.vite/libs/frontend/hud-event-watcher",
+
+    plugins: [nxViteTsPaths(), vue()],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+
+    test: {
+        globals: true,
+        cache: { dir: "../../../node_modules/.vitest" },
+        environment: "jsdom",
+        include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        reporters: ["default"],
+        coverage: { reportsDirectory: "../../../coverage/libs/frontend/hud-event-watcher", provider: "v8" },
+    },
+});
